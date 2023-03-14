80bbb5d00fe8a330c00f255b934e104f611654ae
Gareth Evans <gareth@bryncynfelin.co.uk>
2023-03-14 10:12:15 +0000

    do not attempt to parse port

--- main.go
+++ main.go
@@ -3,14 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 	"os"
-	"strconv"
-
-	"github.com/go-chi/chi/v5/middleware"
 )
 
-const defaultPort = 3000
+const defaultPort = "3000"
 
 func main() {
 	r := chi.NewRouter()
@@ -18,17 +16,13 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
-	http.ListenAndServe(fmt.Sprintf(":%d", port()), r)
+	http.ListenAndServe(fmt.Sprintf(":%s", port()), r)
 }
 
-func port() int {
+func port() string {
 	s := os.Getenv("PORT")
 	if s == "" {
 		return defaultPort
 	}
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		panic(err)
-	}
-	return i
+	return s
 }
